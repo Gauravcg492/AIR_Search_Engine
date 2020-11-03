0b78f87e0a1008eea20bb2a5d5357a5c72a45149
anagha1999 <anagha.ananth7@gmail.com>
2020-11-03 09:23:06 +0530

    token normalization done

--- inverted-index/token-gen.py
+++ inverted-index/token-gen.py
@@ -1,81 +1,75 @@
 import nltk
 import json
-dataset = "final.json"
-file = open("term_doc_id_pos", "w")
-mapper = open("mappings", "w")
+import string
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+from nltk.corpus import stopwords
+lemmatizer = WordNetLemmatizer()
+
+dataset = "../../dataset/final.json"
+file = open("../../temp-data/term_doc_id_pos", "w") #intermediate
+mapper = open("../doc_id_url_mappings", "w") #required globally
 count = 0 
 temp = 1
+
+# function to convert nltk tag to wordnet tag
+def nltk_tag_to_wordnet_tag(nltk_tag):
+    if nltk_tag.startswith('J'):
+        return wordnet.ADJ
+    elif nltk_tag.startswith('V'):
+        return wordnet.VERB
+    elif nltk_tag.startswith('N'):
+        return wordnet.NOUN
+    elif nltk_tag.startswith('R'):
+        return wordnet.ADV
+    else:          
+        return None
+
+def lemmatize(token):
+    #tokenize the sentence and find the POS tag for each token
+    nltk_tagged = nltk.pos_tag([token])  
+    #tuple of (token, wordnet_tag)
+    wordnet_tagged = map(lambda x: (x[0], nltk_tag_to_wordnet_tag(x[1])), nltk_tagged)
+    for word, tag in wordnet_tagged:
+        if tag is None:
+            #if there is no available tag, return word as is
+            return word
+        else:        
+            #else use the tag to lemmatize the token
+            lemmatized_word = lemmatizer.lemmatize(word, tag)
+            return lemmatized_word
+
 for line in open(dataset):
+		# TEST COMMENT BEGINS HERE
 		# if(count==3):
 		# 	break
 		# count+=1
-		# print(line)
-		record = json.loads(line)
-		# can we assign a numeric doc id to each record
-		# need to store doc-id => URL mapping 
+		# TEST COMMENT ENDS HERE
+
+		record = json.loads(line) 
 		curr_doc_url = record["\ufeffURL"]
 		curr_doc_id = temp
 		mapper.write(str(curr_doc_id)+','+str(curr_doc_url)+'\n')
 		del record["\ufeffURL"]
-		# for el in record.values():
 		for el in record.keys():
 			pos_counter = 0
 			value = record[el]
 			tokens = nltk.word_tokenize(value)
 			for token in tokens:
-				#print(tokens.index(token))
+				# add code to ignore stop words here
+				# handle pos_counter accordingly
+				token=token.replace('-','')
+				if(token=='' or token =="'s" or (str(token) in string.punctuation)):
+					#ignoring punctuation tokens
+					continue
+				if (str(token) in stopwords.words('english')):
+					# removing stop words but retaining their position
+					pos_counter+=1
+					continue
+				token = lemmatize(token)
 				file.write(token +","+ str(curr_doc_id) + "," + str(pos_counter) + "," + str(el) + "\n")
 				pos_counter+=1
-				# file.write(token +","+ str(curr_doc_id) + "," + str(tokens.index(token)) + "," + str(el) + "\n")
+
 		temp+=1
 		file.write("\n")
 
-# For reference, here is the post-processing result for record #1
-# 12/27/2010,1,0,MatchDateTime
-# 19:20:53,1,1,MatchDateTime
-# MSNBC,1,0,Station
-# News,1,0,Show
-# Nation,1,1,Show
-# MSNBC_20101227_190000_News_Nation,1,0,IAShowID
-# https,1,0,IAPreviewThumb
-# :,1,1,IAPreviewThumb
-# //archive.org/download/MSNBC_20101227_190000_News_Nation/MSNBC_20101227_190000_News_Nation.thumbs/MSNBC_20101227_190000_News_Nation_001225.jpg,1,2,IAPreviewThumb
-# to,1,0,Snippet
-# blame,1,1,Snippet
-# .,1,2,Snippet
-# with,1,3,Snippet
-# me,1,4,Snippet
-# is,1,5,Snippet
-# a,1,6,Snippet
-# doctor,1,7,Snippet
-# from,1,8,Snippet
-# the,1,9,Snippet
-# pew,1,10,Snippet
-# center,1,11,Snippet
-# on,1,12,Snippet
-# global,1,13,Snippet
-# climate,1,14,Snippet
-# change,1,15,Snippet
-# .,1,16,Snippet
-# you,1,17,Snippet
-# look,1,18,Snippet
-# at,1,19,Snippet
-# tennessee,1,20,Snippet
-# and,1,21,Snippet
-# the,1,22,Snippet
-# wicked,1,23,Snippet
-# flooding,1,24,Snippet
-# they,1,25,Snippet
-# had,1,26,Snippet
-# there,1,27,Snippet
-# .,1,28,Snippet
-# this,1,29,Snippet
-# has,1,30,Snippet
-# been,1,31,Snippet
-# --,1,32,Snippet
-# or,1,33,Snippet
-# the,1,34,Snippet
-# global,1,35,Snippet
-# warming,1,36,Snippet
-# system,1,37,Snippet
-# .,1,38,Snippet
