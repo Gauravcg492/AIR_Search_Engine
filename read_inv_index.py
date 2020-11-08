8f3b581d000c3217db416d2bd2c5521de6c96f8f
KARTIKEYA <kartikeya@KARTIKEYAs-MacBook-Pro-4.local>
2020-11-09 01:13:08 +0530

    Added read_inv_index
    Added read_inv_index with the changes, returns docid, row and data

--- read_inv_index.py
+++ read_inv_index.py
@@ -1,30 +1,17 @@
 import json
+import pandas as pd
+
 def get_inv_index():
 	f = open("../inv_index.json"):
 	inv_index = json.load(f)
 	return inv_index
 
-'''def get_idf(term):
-	return record[get_term(record)][0]
+def get_record(doc_id):
+	dataset = "../dataset/TelevisionNews/"
+	filename, row_no =doc_id.split("_")
+	df = pd.read_csv(dataset+filename+".csv")
+	record = dict(df.iloc[int(row_no)])
+	return (filename+".csv", row_no, record)
 
-def get_postings_list(record):
-	return record[get_term(record)][1]
 
-def get_champ_list(record):
-	return record[get_term(record)][2]
 
-def get_zones_list(record):
-	return record[get_term(record)][2]
-'''
-
-'''
-DRIVER CODE:	
-for line in open("../inv_index.json"):
-	record = json.loads(line)
-	any-of-above-functions(record)
-
-NOTE:
-- record is a dictionary
-- record has only one key(term) with its corresponding value (idf, postings list, champion list)
-
-'''
