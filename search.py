59ac4a04a8d26af260de3d66532f49a4885d954a
kamath-cell <65301154+kamath-cell@users.noreply.github.com>
2020-11-13 12:29:27 +0530

    Update search.py

--- search.py
+++ search.py
@@ -60,25 +60,11 @@ class Search:
                 self.wildcard_index[term] = intersect.merge_terms(self.wildcard_index[term], self.inv_ind[wild_term])
 
 
-
-
-
-
-
-
-
-
-
     def return_documents(self, scores):
         result = []
-        temp = scores#[::-1]
-        for score in temp:
-            result.append(get_record(score[1], score[0]))
-        for score in reversed(scores[:20]):
-            result.append(get_record(score[1]))
         temp = scores[::-1]
         for score in temp[:self.k]:
-            result.append(get_record(score[1]))
+            result.append(get_record(score[1],score[0]))
         return result
 
     def search(self, query_text):
@@ -94,7 +80,6 @@ class Search:
                 # TODO
                 wildcard_terms = self.get_wild_query(term) #get all terms for mon*
                 self.merge_terms(wildcard_terms, term) #should give new idf, champion list, zones list for wildcard query(eg:- 'mon*')
-                print(wildcard_terms)
             elif term not in self.inv_ind:
                 term = self.spelling_correction(term)
 
