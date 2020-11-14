dffe951cd40a730ebf2726b874585a2bcc338d4b
gauravcg492 <officialgauravcg492@gmail.com>
2020-11-14 18:26:47 +0530

    Reduces multiple cosine calculations, testing remaining

--- search.py
+++ search.py
@@ -17,16 +17,20 @@ class Search:
         self.k = no_docs
     
     # Function which intersects the terms in query and then score the intersected documents
-    def intersect_score(self):
+    def intersect_doc_list(self):
         doc_list = intersect.posinter_over_invindex(self.inv_ind, self.query_terms, self.wildcard_index)
         if(len(doc_list)<self.k):
             doc_list = intersect.posinter_over_invindex(self.inv_ind, self.query_terms, self.wildcard_index, False)
-        return cosine_scoring.get_cosine_score(self.inv_ind, self.wildcard_index, self.query_terms, doc_list[:20])
+        #return cosine_scoring.get_cosine_score(self.inv_ind, self.wildcard_index, self.query_terms, doc_list[:20])
+        return doc_list
     
     # Function which finds cosine score from champion list of the queries
-    def cosine_score_all(self, champion=True):
-        doc_list = intersect.merge_docs(self.inv_ind, self.query_terms, self.wildcard_index, champion)
-        return cosine_scoring.get_cosine_score(self.inv_ind, self.wildcard_index, self.query_terms, doc_list[:20])
+    def union_doc_list(self):
+        doc_list = intersect.merge_docs(self.inv_ind, self.query_terms, self.wildcard_index, champion_list=True)
+        if len(doc_list) < self.k:
+            doc_list = intersect.merge_docs(self.inv_ind, self.query_terms, self.wildcard_index, champion_list=False)
+        #return cosine_scoring.get_cosine_score(self.inv_ind, self.wildcard_index, self.query_terms, doc_list[:20])
+        return doc_list
 
     def spelling_correction(self, term):
         term_bi_len = len(term)-1
@@ -68,15 +72,14 @@ class Search:
 
     def search(self, query_text):
         query_terms = []
-        position = -1
+        #position = -1
         self.wildcard_index = {} #for wildcard query term
         for term in nltk.word_tokenize(query_text):
             term = token_gen.normalize(term)
-            position+=1
+            #position+=1
             if(term ==''):
                 continue
             if('*' in term):
-                # TODO
                 wildcard_terms = self.get_wild_query(term) #get all terms for mon*
                 self.merge_terms(wildcard_terms, term) #should give new idf, champion list, zones list for wildcard query(eg:- 'mon*')
             elif term not in self.inv_ind:
@@ -84,7 +87,8 @@ class Search:
 
             query_terms.append(term)
 
-        self.query_terms = query_terms[1:]
+        self.query_terms = query_terms # Why was it query_term[1:] ?
+        '''
         cosine_score1=self.intersect_score()
         if(len(cosine_score1)> self.k):
             result = self.return_documents(cosine_score1)
@@ -97,5 +101,11 @@ class Search:
                 cosine_score3 = self.cosine_score_all(champion=False)
                 total_cosine_score = sorted(set(total_cosine_score + cosine_score3))
                 result = self.return_documents(total_cosine_score)
+        '''
+        doc_list = self.intersect_doc_list()
+        if len(doc_list) < self.k:
+            doc_list = self.union_doc_list()
+        cosine_scores = cosine_scoring.get_cosine_score(self.inv_ind, self.wildcard_index, self.query_terms, doc_list)
+        result = self.return_documents(cosine_scores)
         return result
             
