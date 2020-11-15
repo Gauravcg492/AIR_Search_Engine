dd2245e1135a6680ce54e1ee1550839be9304926
gauravcg492 <officialgauravcg492@gmail.com>
2020-11-15 19:37:12 +0530

    Added support to save file

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ import flask
 from flask import jsonify
 from search import Search
 from index import Index
+import json
 
 kgram_path = '../k_gram.json'
 inv_ind_path = '../inv_index.json'
@@ -14,10 +15,15 @@ print("Done Indexing")
 @app.route('/', methods=['GET'])
 def search_engine():
 	query = str(flask.request.args.get("q"))
+	save = int(flask.request.args.get("save"))
 	no_docs = 10
 	print("Query:", query)
 	engine = Search(index, no_docs)
 	result = engine.search(query)
+	if save == 1:
+		with open('./' + query + '.json') as out:
+			json.dump(result, out)
+			out.write('\n')
 	return jsonify(result)
 
 if __name__ == "__main__":
